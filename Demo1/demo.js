a01ac4617c5e08b483acb7976a0bdb50877b5484
Da Xu <xuda980520@gmail.com>
2019-11-23 11:04:30 -0500

    add window to demo

---
+++ Demo1/demo.js
@@ -0,0 +1,110 @@
+var win_size_dom = document.querySelector('.window_size');
+
+var win_size = {
+    width: document.getElementById('a').style.width,
+    height: document.getElementById('a').style.height
+};
+
+var initialStatus = {
+    targets: win_size,
+    width: 400,
+    height: 600,
+    round: 1,
+    easing: 'linear',
+    update: function() {
+        win_size_dom.innerHTML = win_size.width + ' * ' + win_size.height;
+    },
+    duration: 800
+};
+var fullScreen = {
+    targets: '.window',
+    left: '0px',
+    width: '1400px',
+    height: '700px',
+    duration: 300,
+    offset: '+=1000'
+};
+var leftHalfScreen = {
+    targets: '.window',
+    left: '0px',
+    width: '700px',
+    height: '700px',
+    duration: 300,
+    offset: '+=1000'
+};
+var rightHalfScreen = {
+    targets: '.window',
+    left: '700px',
+    duration: 300,
+    offset: '+=1000'
+};
+var randomPosition1 = {
+    targets: '.window',
+    width: '200px',
+    height: '200px',
+    duration: 300,
+    offset: '+=1000'
+};
+var randomPosition2 = {
+    targets: '.window',
+    left: '200px',
+    duration: 300,
+    offset: '+=1000',
+
+};
+
+var win_sizeChange_fullScreen = {
+    targets: win_size,
+    width: 1400,
+    height: 700,
+    round: 1,
+    easing: 'linear',
+    update: function() {
+        win_size_dom.innerHTML = win_size.width + ' * ' + win_size.height;
+    }
+};
+var win_sizeChange_halfScreen = {
+    targets: win_size,
+    width: 700,
+    height: 700,
+    round: 1,
+    easing: 'linear',
+    update: function() {
+        win_size_dom.innerHTML = win_size.width + ' * ' + win_size.height;
+    }
+};
+var win_sizeChange_200Screen = {
+    targets: win_size,
+    width: 200,
+    height: 200,
+    round: 1,
+    easing: 'linear',
+    update: function() {
+        win_size_dom.innerHTML = win_size.width + ' * ' + win_size.height;
+    }
+};
+
+var seekProgress = document.querySelector('.progress');
+const tl = anime.timeline({
+    autoplay: false,
+    easing: 'easeInOutQuad',
+    update: function(anim) {
+        seekProgress.value = tl.progress;
+    }
+});
+
+tl.add(initialStatus)
+    .add(fullScreen)
+    .add(win_sizeChange_fullScreen)
+    .add(leftHalfScreen)
+    .add(win_sizeChange_halfScreen)
+    .add(rightHalfScreen)
+    .add(randomPosition1)
+    .add(win_sizeChange_200Screen)
+    .add(randomPosition2);
+
+seekProgress.oninput = function() {
+    tl.seek(tl.duration * (seekProgress.value / 100));
+};
+
+document.getElementById('play').onclick = tl.play;
